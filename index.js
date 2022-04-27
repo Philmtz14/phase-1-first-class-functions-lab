5c27bd9e35af6c89b37291de47e1017d7af565ec
philip martinez <martinez.philip00@gmail.com>
2022-04-27 14:39:56 -0500

    commit tree 471021caf7db1b3ff92c57784819a506a7151279

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+const returnFirstTwoDrivers = function(x){
+    return x.slice(0,2);
+};
+
+const returnLastTwoDrivers = function(x){
+    return x.slice(-2);
+};
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier (multiplier) {
+    return function (fare) {
+        return fare * multiplier
+    }
+}
+
+const fareDoubler = createFareMultiplier(2)
+
+const fareTripler = createFareMultiplier(3)
+
+function selectDifferentDrivers (arrayOfDrivers, selectingDrivers) {
+    return selectingDrivers(arrayOfDrivers)
+}
+
+console.log(selectDifferentDrivers(drivers, returnFirstTwoDrivers))
